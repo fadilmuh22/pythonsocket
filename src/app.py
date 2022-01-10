61a513b533916bbd4b35029dc44dd18523513504
fadilmuh22 <fadilmuh2002@gmail.com>
2022-01-10 10:25:38 +0700

    fixing socket to receive more than 1 command

--- src/app.py
+++ src/app.py
@@ -11,34 +11,34 @@ server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 server.bind(socket_path)
 
 print("Socket bind on {}".format(socket_path))
+server.listen(1)
+conn, addr = server.accept()
+
 while True:
-    server.listen(1)
-    conn, addr = server.accept()
     datas = conn.recv(1024).decode('utf-8')
     if datas:
-        print("Datas received : {}".format(datas.strip()))
-        for data in datas.strip().split("\n"):
-            try:
-                response_data = {}
-                parsed_data = json.loads(data)
+        print("\n\nDatas received : {}\n".format(datas.strip()))
+        try:
+            response_data = {}
+            parsed_data = json.loads(datas.strip())
 
-                response_data[parsed_data['id']] = []
+            response_data[parsed_data['id']] = []
 
-                for i in range(parsed_data['from'], parsed_data['to']+1):
-                    if i % 3 == 0 and i % 5 == 0:
-                        response_data[parsed_data['id']].append(
-                            parsed_data['fizz'] + parsed_data['buzz'])
-                    elif i % 3 == 0:
-                        response_data[parsed_data['id']].append(
-                            parsed_data['fizz'])
-                    elif i % 5 == 0:
-                        response_data[parsed_data['id']].append(
-                            parsed_data['buzz'])
-                    else:
-                        response_data[parsed_data['id']].append(i)
+            for i in range(parsed_data['from'], parsed_data['to']+1):
+                if i % 3 == 0 and i % 5 == 0:
+                    response_data[parsed_data['id']].append(
+                        parsed_data['fizz'] + parsed_data['buzz'])
+                elif i % 3 == 0:
+                    response_data[parsed_data['id']].append(
+                        parsed_data['fizz'])
+                elif i % 5 == 0:
+                    response_data[parsed_data['id']].append(
+                        parsed_data['buzz'])
+                else:
+                    response_data[parsed_data['id']].append(i)
 
-                res_str = json.dumps(response_data)+"\n"
-                conn.send(res_str.encode())
-                print("Datas sended : {}".format(res_str))
-            except:
-                print("String could not be converted to JSON.")
+            res_str = json.dumps(response_data)+"\n"
+            conn.send(res_str.encode())
+            print("Datas sended : {}".format(res_str))
+        except:
+            print("String could not be converted to JSON.")
