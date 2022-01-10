c179ec66493fe79c6b3dc51dee38e0b0423eb656
fadilmuh22 <fadilmuh2002@gmail.com>
2022-01-10 11:24:40 +0700

    change image name and added threading

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import json
+from threading import *
 
 socket_path = os.environ['SOCKET_FILE']
 
@@ -11,34 +12,49 @@ server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 server.bind(socket_path)
 
 print("Socket bind on {}".format(socket_path))
-server.listen(1)
-conn, addr = server.accept()
 
-while True:
-    datas = conn.recv(1024).decode('utf-8')
-    if datas:
-        print("\n\nDatas received : {}\n".format(datas.strip()))
-        try:
-            response_data = {}
-            parsed_data = json.loads(datas.strip())
 
-            response_data[parsed_data['id']] = []
+class Client(Thread):
+    def __init__(self, socket, address):
+        Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
 
-            for i in range(parsed_data['from'], parsed_data['to']+1):
-                if i % 3 == 0 and i % 5 == 0:
-                    response_data[parsed_data['id']].append(
-                        parsed_data['fizz'] + parsed_data['buzz'])
-                elif i % 3 == 0:
-                    response_data[parsed_data['id']].append(
-                        parsed_data['fizz'])
-                elif i % 5 == 0:
-                    response_data[parsed_data['id']].append(
-                        parsed_data['buzz'])
-                else:
-                    response_data[parsed_data['id']].append(i)
+    def run(self):
+        while 1:
+            datas = self.sock.recv(1024).decode('utf-8')
+            if datas:
+                print("\n\nDatas received : {}\n".format(datas.strip()))
+                for data in datas.strip().split("\n"):
+                    try:
+                        response_data = {}
+                        parsed_data = json.loads(data)
 
-            res_str = json.dumps(response_data)+"\n"
-            conn.send(res_str.encode())
-            print("Datas sended : {}".format(res_str))
-        except:
-            print("String could not be converted to JSON.")
+                        response_data[parsed_data['id']] = []
+
+                        for i in range(parsed_data['from'], parsed_data['to']+1):
+                            if i % 3 == 0 and i % 5 == 0:
+                                response_data[parsed_data['id']].append(
+                                    parsed_data['fizz'] + parsed_data['buzz'])
+                            elif i % 3 == 0:
+                                response_data[parsed_data['id']].append(
+                                    parsed_data['fizz'])
+                            elif i % 5 == 0:
+                                response_data[parsed_data['id']].append(
+                                    parsed_data['buzz'])
+                            else:
+                                response_data[parsed_data['id']].append(i)
+
+                        res_str = json.dumps(response_data)+"\n"
+                        self.sock.send(res_str.encode())
+                        print("Datas sended : {}".format(res_str))
+                    except:
+                        print("String could not be converted to JSON.")
+
+
+server.listen()
+print('Server started and listening')
+while 1:
+    clientsocket, address = server.accept()
+    Client(clientsocket, address)
